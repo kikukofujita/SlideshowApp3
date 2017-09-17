854991b120f0e1d56ba56563d9748aedfff48055
kiki.fuji <fujitakikuko@fujitakikuko-no-MacBook-Air.local>
2017-09-18 01:02:33 +0900

    Initial Commit

---
+++ SlideshowApp3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SlideshowApp3
+//
+//  Created by 藤田貴久子 on 2017/09/18.
+//  Copyright © 2017年 kiki.fuji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
